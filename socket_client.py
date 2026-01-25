e3ee1c6d8f67442b737b08feede45ba9f72e9725
da21nny <edgarv21.ev@gmail.com>
2026-01-25 16:29:10 -0300

    correcciones y mejoras

--- socket_client.py
+++ socket_client.py
@@ -1,48 +1,60 @@
 import socket
 import threading
 
-def receive_message(client):
+# Funcion para recibir mensajes del server.
+def receive_message(client_socket):
     while True:
         try:
-            message = client.recv(1024).decode("utf-8")
+            message = client_socket.recv(1024).decode("utf-8")
             if message:
                 print("\n" + message)
             else:
+                print("\n Cerrando Conexion")
+                client_socket.close()
                 break
 
         except:
+            print("\n Error: Se perdio conexion con el servidor")
+            client_socket.close()
             break
 
+# Funcion para enviar mensajes al server.
+def send_message(client_socket):
+    while True:
+        message = input("Ingrese mensaje: ")
 
+        if message.lower() == "salir":
+            print("saliendo del server")
+            break
+        
+    client_socket.send(message.encode("utf-8"))
+
+# Funcion principal donde se conecta al server y crear hilos de envio y recibo de mensajes.
 def main():
     server_ip = "127.0.0.1"
     server_port = 8000
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-    client.connect((server_ip, server_port))
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+    client_socket.connect((server_ip, server_port))
     print("Conectado al server")
 
     try:
         name = input("Introduce tu nick: ")
-        client.send(name.encode("utf-8"))
+        client_socket.send(name.encode("utf-8"))
 
-        client_thread = threading.Thread(target=receive_message, args=(client,))
-        client_thread.daemon = True
-        client_thread.start()
+        thread_send = threading.Thread(target=send_message, args=(client_socket,))
+        thread_send.daemon = True
+        thread_send.start()
 
-        while True:
-            message = input("Ingrese mensaje: ")
+        thread_receive = threading.Thread(target=receive_message, args=(client_socket,))
+        thread_receive.daemon = True
+        thread_receive.start()
 
-            if message.lower() == "salir":
-                print("saliendo del server")
-                break
-        
-            client.send(message.encode("utf-8"))
             
     except Exception as e:
         print(f"Error: {e}")
 
     finally:
-        client.close()
+        client_socket.close()
         print("desconectado")
 
-main()
+main() # Ejecuta el main principal.
